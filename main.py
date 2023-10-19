f413b5c8245414ba825f693d4c6918df2d01848b
Jebin_3 <90751436+jebin232@users.noreply.github.com>
2023-10-19 05:43:06 -1200

    main.py

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
 from rembg import remove
 from PIL import Image
 import io
 
 app = FastAPI()
 
-# CORS Configuration
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://groun.onrender.com"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 @app.post("/remove_background/")
-async def remove_background(file: UploadFile):
-    # Check if a file was provided
-    if not file:
-        return {"error": "No file provided."}
+async def remove_background(image: UploadFile):
+    # Check if the uploaded file is an image
+    if not image.content_type.startswith("image/"):
+        return {"error": "Invalid file type. Please upload an image."}
 
     # Read the image file
-    image_bytes = await file.read()
+    image_bytes = await image.read()
+    image_data = Image.open(io.BytesIO(image_bytes))
 
-    # Perform background removal
-    image = Image.open(io.BytesIO(image_bytes))
-    output_image = remove(image)
+    # Remove the background
+    output_image = remove(image_data)
 
-    # Save the output image to a BytesIO object
+    # Save the processed image to a temporary file
     output_buffer = io.BytesIO()
     output_image.save(output_buffer, format="PNG")
-    output_bytes = output_buffer.getvalue()
+    output_buffer.seek(0)
 
-    # Return the resulting image
-    return {"result_image": output_bytes}
+    return {
+        "message": "Background removed successfully.",
+        "processed_image": output_buffer,
+    }
